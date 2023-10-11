75f5dbfcf479db48b0f5f1792225bd1f7ac6c432
Jared Morillo <jared.morillo@hotmail.com>
2023-10-11 10:09:34 -0500

    bom

--- lesson05/script/bom.js
+++ lesson05/script/bom.js
@@ -1,6 +1,32 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+const chapterList = []; // Array to store entered chapters
 
-button.addEventListener('click', function(){button});
-button.addEventListener('click', () =>);
+button.addEventListener('click', () => {
+  const chapter = input.value.trim(); // Remove leading and trailing whitespace
+
+  if (chapter !== '' && !chapterList.includes(chapter)) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = chapter;
+    deleteButton.textContent = '❌';
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      chapterList.splice(chapterList.indexOf(chapter), 1); // Remove chapter from the list
+      input.focus();
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    chapterList.push(chapter); // Add chapter to the list
+    input.value = '';
+    input.focus();
+  } else if (chapter === '') {
+    alert('Please enter a chapter.');
+  } else {
+    alert('Chapter already exists.');
+  }
+});
