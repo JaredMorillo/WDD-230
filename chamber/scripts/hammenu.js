0baf00a50798f9d0d65c70a798200fe57e6450ef
Jared Morillo <jared.morillo@hotmail.com>
2023-10-18 08:19:31 -0500

    Chamber

---
+++ chamber/scripts/hammenu.js
@@ -0,0 +1,20 @@
+/*Animation Page*/
+var myVar;
+    
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+/*Hambuger Button*/
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
