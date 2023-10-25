255f40ed40d36682ff4d6eccd4abb5503a64385f
Jared Morillo <jared.morillo@hotmail.com>
2023-10-25 12:20:21 -0500

    Chamber

--- chamber/scripts/hammenu.js
+++ chamber/scripts/hammenu.js
@@ -9,6 +9,13 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+//Weather
+var temp= 57;
+var wSpeed= 15;
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChill= Math.round(windChill);
+document.getElementById("windChill").innerHTML= windChill;
 
 /*Hambuger Button*/
 const hamButton = document.querySelector('#menu');
@@ -18,3 +25,4 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
